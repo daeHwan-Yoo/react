efb2549be2df1d0436198338bbf1a14006c25d8a
Daehwan Yoo <ydh05051@gmail.com>
2022-06-28 22:16:03 +0900

    re-commit EventPractice.js

---
+++ src/EventPractice.js
@@ -0,0 +1,50 @@
+import { Component } from "react";
+
+class EventPractice extends Component {
+  state = {
+    message: "",
+  };
+
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      message: e.target.value,
+    });
+  }
+
+  handleClick() {
+    alert(this.state.message);
+    this.setState({
+      message: "",
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>event practice</h1>
+        <input
+          type="text"
+          name="message"
+          placeholder="type anything"
+          value={this.state.message}
+          onChange={this.handleChange}
+        />
+        <button
+          onClick={() => {
+            this.handleClick();
+          }}
+        >
+          check
+        </button>
+      </div>
+    );
+  }
+}
+
+export default EventPractice;
