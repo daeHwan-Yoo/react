b9f14968b82fc4b0618d7cb7000cd8fdd070ddc6
Daehwan Yoo <ydh05051@gmail.com>
2022-07-06 20:07:12 +0900

    chapter 5, ref

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import Counter from "./Counter";
-import EventPractice from "./EventPractice";
+import {Component} from 'react';
+import ScrollBox from './ScrollBox';
 
-const App=()=>{
-  return <EventPractice />
-};
+class App extends Component{
+  render(){
+    return(
+      <div>
+        <ScrollBox ref={(ref)=>this.scrollBox=ref}/>
+        <button onClick={()=>this.scrollBox.scrollToBottom()}>
+          to the bottom
+        </button>
+      </div>
+    );
+  }
+}
 
 export default App;
 
