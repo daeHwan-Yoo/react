b0d04f3b7499f6c091eb5efbc6ba2b8aec3f8c21
Daehwan Yoo <ydh05051@gmail.com>
2022-07-12 12:48:33 +0900

    Chapter 6 completed

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -1,25 +1,46 @@
-import {useState} from 'react';
+import { useState } from "react";
 
-const IterationSample=()=>{
-    const [names, setNames]=useState([
-        {id:1, text:'snowman'},
-        {id:2, text: 'snow'},
-        {id:3, text:'snow'},
-        {id:4, text:'wind'}
-    ]);
-    
-    const [inputText, setInputText]=useState('');
-    const [nextId, setNextId]=useState(5);
+const IterationSample = () => {
+  const [names, setNames] = useState([
+    { id: 1, text: "snowman" },
+    { id: 2, text: "snow" },
+    { id: 3, text: "snow" },
+    { id: 4, text: "wind" },
+  ]);
 
-    const onChange=e=>setInputText(e.target.value);
+  const [inputText, setInputText] = useState("");
+  const [nextId, setNextId] = useState(5);
 
-    const namesList=names.map((name, index)=><li key={name.id}>{name.text}</li>);
-    return <>
-        <input value={inputText} onChange={onChange} />
-        <button>add</button>
-        <ul>{namesList}</ul>
-    </>;
+  const onChange = (e) => setInputText(e.target.value);
+
+  const onClick = () => {
+    const nextNames = names.concat({
+      id: nextId,
+      text: inputText,
+    });
+    setNextId(nextId + 1);
+    setNames(nextNames);
+    setInputText("");
+  };
+
+  const onRemove = (id) => {
+    const nextNames = names.filter((name) => name.id !== id);
+    setNames(nextNames);
+  };
+
+  const namesList = names.map((name) => (
+    <li key={name.id} onDoubleClick={() => onRemove(name.id)}>
+      {name.text}
+    </li>
+  ));
+
+  return (
+    <>
+      <input value={inputText} onChange={onChange} />
+      <button onClick={onClick}>add</button>
+      <ul>{namesList}</ul>
+    </>
+  );
 };
 
 export default IterationSample;
-
